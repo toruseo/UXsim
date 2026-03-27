31068f668936ca00e54a0949fa00777297cf42aa
Toru Seo <34780089+toruseo@users.noreply.github.com>
2026-03-27 12:04:27 +0900

    Add test for no cyclic path

--- tests/test_verification_route_choice.py
+++ tests/test_verification_route_choice.py
@@ -934,3 +934,100 @@ def test_route_choice_dynamic_congestion_pricing():
     assert eq_tol(linkB.average_flow(2000), 0.6)
     assert eq_tol(linkB.average_flow(3000), 0.6)
     assert eq_tol(linkB.average_flow(4500), 0.0)
+
+@pytest.mark.flaky(reruns=5)
+def test_route_choice_no_cyclic_path():
+    W = World(
+        name="",
+        deltan=10,
+        tmax=7200,
+        print_mode=1, save_mode=0, show_mode=1,
+        random_seed=None,
+        no_cyclic_path=True
+    )
+
+    # scenario
+    #automated network generation
+    #deploy nodes as an imax x jmax grid
+    imax = 13
+    jmax = 13
+    nodes = {}
+    for i in range(imax):
+        for j in range(jmax):
+            nodes[i,j] = W.addNode(f"n{(i,j)}", i, j)
+
+    #create links between neighborhood nodes
+    links = {}
+    for i in range(imax):
+        for j in range(jmax):
+            if i != imax-1:
+                links[i,j,i+1,j] = W.addLink(f"l{(i,j,i+1,j)}", nodes[i,j], nodes[i+1,j], length=1000, free_flow_speed=10, jam_density=0.2)
+            if i != 0:
+                links[i,j,i-1,j] = W.addLink(f"l{(i,j,i-1,j)}", nodes[i,j], nodes[i-1,j], length=1000, free_flow_speed=10, jam_density=0.2)
+            if j != jmax-1:
+                links[i,j,i,j+1] = W.addLink(f"l{(i,j,i,j+1)}", nodes[i,j], nodes[i,j+1], length=1000, free_flow_speed=10, jam_density=0.2)
+            if j != 0:
+                links[i,j,i,j-1] = W.addLink(f"l{(i,j,i,j-1)}", nodes[i,j], nodes[i,j-1], length=1000, free_flow_speed=10, jam_density=0.2)
+
+    #generate traffic demand between the boundary nodes
+    demand_flow = 0.025
+    demand_duration = 3600
+    for n1 in [(0,j) for j in range(jmax)]:
+        for n2 in [(imax-1,j) for j in range(jmax)]:
+            W.adddemand(nodes[n2], nodes[n1], 0, demand_duration, demand_flow)
+            W.adddemand(nodes[n1], nodes[n2], 0, demand_duration, demand_flow)
+    for n1 in [(i,0) for i in range(imax)]:
+        for n2 in [(i,jmax-1) for i in range(imax)]:
+            W.adddemand(nodes[n2], nodes[n1], 0, demand_duration, demand_flow)
+            W.adddemand(nodes[n1], nodes[n2], 0, demand_duration, demand_flow)
+            
+    # execute simulation
+    W.exec_simulation()
+
+    # visualize
+    W.analyzer.print_simple_stats()
+
+
+    # count vehicles that use the same link or inverse link during 1 trip
+    print("\nStats for same or inverse link traveling")
+
+    n_same = 0
+    n_inv = 0
+    n_all = 0
+
+    n_same_veh = 0
+    n_inv_veh = 0
+
+    for veh in W.VEHICLES.values():
+        route_nodes = [(l.start_node, l.end_node) for l in veh.traveled_route()[0]]
+        n_same_veh_tmp = 0
+        n_inv_veh_tmp = 0
+        for i in range(len(route_nodes)):
+            for j in range(i+1, len(route_nodes)):
+                ll = route_nodes[i]
+                kk = route_nodes[j]
+                if ll == kk:
+                    n_same += 1
+                    n_same_veh_tmp = 1
+                if (ll[0], ll[1]) == (kk[1], kk[0]):
+                    n_inv += 1
+                    n_inv_veh_tmp = 1
+
+            n_all += 1
+        n_same_veh += n_same_veh_tmp
+        n_inv_veh += n_inv_veh_tmp
+            
+    print(
+        f"all_links={n_all:}\n"
+        f"same_link={n_same:} ({n_same / n_all:.2%})\n"
+        f"inverse_link={n_inv:} ({n_inv / n_all:.2%})\n"
+    )
+
+    print(
+        f"vehicles={len(W.VEHICLES):}\n"
+        f"same_link={n_same_veh:} ({n_same_veh / len(W.VEHICLES):.2%})\n"
+        f"inverse_link={n_inv_veh:} ({n_inv_veh / len(W.VEHICLES):.2%})\n"
+    )
+
+    assert n_same == 0
+    assert n_inv == 0
